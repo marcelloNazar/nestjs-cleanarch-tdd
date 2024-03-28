bcb1e343957c912db621eba1c561d702966463de
Marcello <marcellonazarb@hotmail.com>
2024-03-28 14:48:47 -0300

    implementing user module and unit tests

---
+++ src/users/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,64 @@
+import { UserEntity, UserProps } from '../../user.entity'
+import { UserDataBuilder } from '../helpers/user-data-builder'
+
+describe('UserEntity unit tests', () => {
+  let props: UserProps
+  let sut: UserEntity
+
+  beforeEach(() => {
+    props = UserDataBuilder({})
+    sut = new UserEntity(props)
+  })
+
+  it('Constructor method', () => {
+    expect(sut.props.name).toEqual(props.name)
+    expect(sut.props.email).toEqual(props.email)
+    expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+
+  it('Getter of name field', () => {
+    expect(sut.props.name).toBeDefined()
+    expect(sut.props.name).toEqual(props.name)
+    expect(typeof sut.props.name).toBe('string')
+  })
+
+  it('Setter of name field', () => {
+    sut['name'] = 'other name'
+    expect(sut.props.name).toEqual('other name')
+    expect(typeof sut.props.name).toBe('string')
+  })
+
+  it('Getter of email field', () => {
+    expect(sut.props.email).toBeDefined()
+    expect(sut.props.email).toEqual(props.email)
+    expect(typeof sut.props.email).toBe('string')
+  })
+
+  it('Getter of password field', () => {
+    expect(sut.props.password).toBeDefined()
+    expect(sut.props.password).toEqual(props.password)
+    expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('Setter of password field', () => {
+    sut['password'] = 'other password'
+    expect(sut.props.password).toEqual('other password')
+    expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('Getter of password field', () => {
+    expect(sut.props.createdAt).toBeDefined()
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+
+  it('Should update a user', () => {
+    sut.update('other name')
+    expect(sut.props.name).toEqual('other name')
+  })
+
+  it('Should update the password field', () => {
+    sut.updatePassword('other password')
+    expect(sut.props.password).toEqual('other password')
+  })
+})
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,50 @@
+import { Entity } from '@/shared/domain/entities/entity'
+
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createdAt?: Date
+}
+
+export class UserEntity extends Entity<UserProps> {
+  constructor(
+    public readonly props: UserProps,
+    id?: string,
+  ) {
+    super(props, id)
+    this.props.createdAt = this.props.createdAt ?? new Date()
+  }
+
+  update(value: string): void {
+    this.name = value
+  }
+
+  updatePassword(value: string): void {
+    this.password = value
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  private set name(value: string) {
+    this.props.name = value
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  private set password(value: string) {
+    this.props.password = value
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+}
