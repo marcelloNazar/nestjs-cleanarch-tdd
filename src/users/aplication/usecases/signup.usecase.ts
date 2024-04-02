8566be0e6c2d9bfdc3904da3d17badbbf91f9c48
Marcello <marcellonazarb@hotmail.com>
2024-04-02 15:24:28 -0300

    adding bcrypt on password and unit tests

---
+++ src/users/aplication/usecases/signup.usecase.ts
@@ -0,0 +1,38 @@
+import { UserRepository } from '@/users/repositories/user.repository'
+import { BadRequestError } from '../errors/bad-request-error'
+import { UserEntity } from '@/users/entities/user.entity'
+
+export namespace SignupUseCase {
+  export type Input = {
+    name: string
+    email: string
+    password: string
+  }
+
+  export type Output = {
+    id: string
+    name: string
+    email: string
+    password: string
+    createdAt: Date
+  }
+
+  export class UseCase {
+    constructor(private userRepository: UserRepository.Repository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const { email, name, password } = input
+
+      if (!email || !name || !password) {
+        throw new BadRequestError('Input data not provided')
+      }
+
+      await this.userRepository.emailExists(email)
+
+      const entity = new UserEntity(input)
+
+      await this.userRepository.insert(entity)
+      return entity.toJSON()
+    }
+  }
+}
